30433635585b98d10bfd559b2c7b8ca7bd7cbd59
ujwala3005 <156195453+ujwala3005@users.noreply.github.com>
2025-12-11 20:32:02 +0530

    Create catalogRestriction.css

---
+++ Groovy/Cute/catalogRestriction.css
@@ -0,0 +1,72 @@
+<style>
+    #backBtn{
+  font-size: 12px;
+    padding: 0 10px;
+    height: 30px;
+    line-height: 30px;
+    text-transform: uppercase;
+    display: inline-block;
+    color: #fff;
+    border-radius: 3px;
+    color: white;
+    font-weight: 501;
+    cursor: pointer;
+    right: 0;
+    top: 24%;
+    text-decoration: none;
+    position: absolute;
+    background: #0052cc;
+    width: 5%;
+}
+h1[data-testid="request-title"] {
+    width: 85%;
+}
+.rw_external_icon_arrow-left{
+
+    position: absolute !important;
+    top: 50px  !important;
+    padding-right: 22px  !important;
+}
+
+
+
+
+</style>
+<script>
+    if (window.location.href.indexOf("portal/7") != -1) {
+  setTimeout(function()
+
+          {  
+    AJS.$(document).ready(function () {
+ //  let div = document.getElementById("content")
+//let icon=document.createElement("a")
+//icon.setAttribute("class","ext_ui_icon rw_external_icon_arrow-left")
+//icon.setAttribute("href","javascript:window.history.back();")
+//icon.setAttribute("id","backBtn")
+//if(document.getElementsByClassName("ext_ui_icon rw_external_icon_arrow-left").length<=0){
+  //div.append(icon);
+  //}
+    });
+    },100)
+  }
+if (window.location.href.indexOf("desk/portal") != -1) {
+if (document.readyState == 'complete') {
+//console.log("hi1")
+   let div = document.getElementById("rw_request_header")
+let icon=document.createElement("a")
+var text = document.createTextNode("Back");
+
+icon.appendChild(text);
+icon.setAttribute("class","ext_ui_icon rw_external_icon_arrow-left")
+icon.setAttribute("href","javascript:window.history.back();")
+icon.setAttribute("id","backBtn")
+if(document.getElementsByClassName("ext_ui_icon rw_external_icon_arrow-left").length<=0){
+ div.append(icon);
+  }
+} else {
+ // console.log("hi222")
+}
+
+}
+
+</script>
