ba02ea72cc35c6397f8c7f1638dacce76eaff870
ujwala3005 <156195453+ujwala3005@users.noreply.github.com>
2025-12-11 20:33:06 +0530

    Create catalogs.js

---
+++ Groovy/Cute/catalogs.js
@@ -0,0 +1,59 @@
+   AJS.$(document).ready(function () {
+     //hardware
+if (window.location.href.indexOf("create/175") != -1) {
+     AJS.$('span.vp-optional').css('display','none')
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+      AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/hardware.png'/>");
+
+     // AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/GSR-final.PNG'/>");
+  
+    });
+  }else{
+    //AJS.$("#content img:last-child").remove();
+  } 
+
+//software
+if (window.location.href.indexOf("create/166") != -1) {
+     AJS.$('span.vp-optional').css('display','none')
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+  AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%' src='/plugins/servlet/desk/resource/global/imagebank/images/software.png'/>");
+
+     // AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/GSR-final.PNG'/>");
+  
+    });
+  }else{
+    //AJS.$("#content img:last-child").remove();
+  } 
+//facility
+if (window.location.href.indexOf("create/178") != -1) {
+     AJS.$('span.vp-optional').css('display','none')
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+        AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%' src='/plugins/servlet/desk/resource/global/imagebank/images/facility.png'/>");
+
+     // AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/GSR-final.PNG'/>");
+  
+    });
+  }else{
+    //AJS.$("#content img:last-child").remove();
+  } 
+//privacy event
+  if (window.location.href.indexOf("create/109") != -1) {
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+       //AJS.$('#customfield_10540-label').append("<img id='info-img' src='https://jiraet-uat.cotiviti.com/plugins/servlet/desk/resource/global/imagebank/images/incidentinfo-contact.png'/>");
+       AJS.$('#content').append("<img id='contact-img' style='width:30%;position: fixed;top: 0;right: 7%' src='/plugins/servlet/desk/resource/global/imagebank/images/PHInstructions.png'/>");
+  
+    });
+  }
+  if (window.location.href.indexOf("create/177") != -1) {
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+       //AJS.$('#customfield_10540-label').append("<img id='info-img' src='https://jiraet-uat.cotiviti.com/plugins/servlet/desk/resource/global/imagebank/images/incidentinfo-contact.png'/>");
+       //AJS.$('#content').append("<img id='contact-img' style='width:30%;position: fixed;top: 0;right: 7%' src='/plugins/servlet/desk/resource/global/imagebank/images/PHInstructions.png'/>");
+  
+    });
+  }
+     });
