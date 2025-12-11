9db9b64688907f63959d92edd7cf1bb0a314fee6
ujwala3005 <156195453+ujwala3005@users.noreply.github.com>
2025-12-11 20:33:40 +0530

    Create createeditIssueLayout.js

---
+++ Groovy/Cute/createeditIssueLayout.js
@@ -0,0 +1,66 @@
+//hardware
+if (window.location.href.indexOf("create/175") != -1) {
+     AJS.$('span.vp-optional').css('display','none')
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+      AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 36%;' src='/plugins/servlet/desk/resource/global/imagebank/images/hardware.png'/>");
+
+     // AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/GSR-final.PNG'/>");
+  
+    });
+  }else{
+    //AJS.$("#content img:last-child").remove();
+  } 
+
+//software
+if (window.location.href.indexOf("create/166") != -1) {
+     AJS.$('span.vp-optional').css('display','none')
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+  AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%' src='/plugins/servlet/desk/resource/global/imagebank/images/software.png'/>");
+
+     // AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/GSR-final.PNG'/>");
+  
+    });
+  }else{
+    //AJS.$("#content img:last-child").remove();
+  } 
+//facility
+if (window.location.href.indexOf("create/178") != -1) {
+     AJS.$('span.vp-optional').css('display','none')
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+        AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%' src='/plugins/servlet/desk/resource/global/imagebank/images/facility.png'/>");
+
+     // AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/GSR-final.PNG'/>");
+  
+    });
+  }else{
+    //AJS.$("#content img:last-child").remove();
+  } 
+//DMARC
+
+
+if (window.location.href.indexOf("create/479") != -1) {
+      AJS.$( document ).ready(function() {  
+
+   setTimeout(function()
+              { 
+    AJS.$('span.vp-optional').css('display','none')
+    
+    }, 100);
+   
+     
+     
+    });
+  
+    AJS.$('#content-wrapper').css({'margin':'0','width':'65%'});
+    AJS.$(document).ready(function () {
+      
+      AJS.$('#content').append("<img id='contact-img' style='position: fixed;top: 0;right: 1%;width: 35%;' src='/plugins/servlet/desk/resource/global/imagebank/images/security.png'/>");
+            AJS.$('#content').append("<div id='cheatSheet' style='position: fixed;top: 80%;left: 66.5%;width: 31%;border: 1px solid gray;padding: 0.4%;'><img id='' style='position: relative;width: 10%;' src='/plugins/servlet/desk/resource/global/imagebank/images/cheatSheetbg.png'/><span style='position:absolute;font-size:16px;font-weight: 500;padding-left:10px'>For more information related to Security Services<br><a href='/plugins/servlet/desk/site/global/article/31490537/Security+Services+Cheat+Sheet'target='_blank'> Click Here</a></span></div>");
+
+    });
+  }else{
+    //AJS.$("#content img:last-child").remove();
+  } 
