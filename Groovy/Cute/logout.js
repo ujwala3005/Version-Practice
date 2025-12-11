d4165c4e02499b888ba2af1de4e71a61b6a16b4f
ujwala3005 <156195453+ujwala3005@users.noreply.github.com>
2025-12-11 20:35:07 +0530

    Create logout.js

---
+++ Groovy/Cute/logout.js
@@ -0,0 +1,6 @@
+jQuery(function($){
+    $('#rw_logout_user_button').click(function() {
+              document.getElementById("rw_logout_user_button").href="https://my-uat.cotiviti.com/login.jsp";
+    });
+    
+});
